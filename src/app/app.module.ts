81d0781576c162425fc018a792eec5c935987c61
Ryan Wolfe <rn.wolfe@gmail.com>
2018-09-05 17:16:47 -0400

    add frontend routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import {
   MatExpansionModule
  } from '@angular/material';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -25,6 +27,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
