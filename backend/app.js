e65324a73bf85bc23d637f7007eb03ad3c68460c
Ryan Wolfe <rn.wolfe@gmail.com>
2018-09-05 16:29:36 -0400

    add delete post feature

--- backend/app.js
+++ backend/app.js
@@ -30,11 +30,13 @@ app.post('/api/posts', (req, res, next) => {
     content: req.body.content
   });
 
-  post.save();
-
-  res.status(201).json({
-    message: 'Post added successfully.'
-  });
+  post.save()
+    .then(createdPost => {
+      res.status(201).json({
+        message: 'Post added successfully.',
+        postId: createdPost._id
+      });
+    });
 });
 
 app.get('/api/posts', (req, res, next) => {
@@ -45,7 +47,14 @@ app.get('/api/posts', (req, res, next) => {
         posts: documents
       });
     });
+});
 
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id })
+    .then(result => {
+      console.log(result);
+      res.status(200).json({ message: 'Post deleted!' });
+    });
 });
 
 app.use('/api/static.txt', (req, res, next) => {
