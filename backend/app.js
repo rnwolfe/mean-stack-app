e39ae48b250a95d9d13524e23a196e7e88df1c3e
Ryan Wolfe <rn.wolfe@gmail.com>
2018-09-05 14:06:36 -0400

    add api endpoints for get/post posts, no db

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/api/posts', (req, res, next) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully.'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     {
       id: '1',
@@ -26,5 +45,10 @@ app.use('/api/posts', (req, res, next) => {
     posts: posts
   });
 });
+
+app.use('/api/static.txt', (req, res, next) => {
+  res.send('this,could,be,a,csv');
+  // how to send newline?
+})
 
 module.exports = app;
